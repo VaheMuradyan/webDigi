4280362e05f6f7c030d8b1a59675f6451aae6344
VaheMuradyan <128795221+VaheMuradyan@users.noreply.github.com>
2023-10-02 11:05:20 -0700

    Add files via upload

---
+++ java7/src/main/java/com/example/java7/NewServlet.java
@@ -0,0 +1,21 @@
+package com.example.java7;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+public class NewServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+
+        writer.write("<html>");
+        writer.write("<H1> Barev Java <H1>");
+        writer.write("</html>");
+    }
+}
